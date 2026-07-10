db05ab085b460cfb0ce1d1fff134e8c4c3d6d133
thangsaoly <lovepapa987@gmail.com>
2026-07-10 13:31:26 +0700

    feat: turn database online

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -26,6 +26,12 @@ const sequelize = new Sequelize(database, user, password, {
   port,
   dialect: 'mysql',
   logging: false,
+  dialectOptions: process.env.NODE_ENV === 'production' ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  } : {},
   pool: {
     max: 10,
     min: 0,
